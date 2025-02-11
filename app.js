808d3cf4050ae0a3cdd772325c45a2729639970f
danielodg <danield317@unitec.edu>
2025-02-11 17:21:26 -0600

    Agregando env

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+require('dotenv').config()  
+console.log(process.env.NEW_API)
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
